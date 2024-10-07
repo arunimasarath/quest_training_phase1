25f4af4cb3d086c17146ff5491fd483a8cacca0f
arunimasarath <arunimasarath2002@gmail.com>
2024-10-07 11:02:19 -0700

    Add files via upload

---
+++ MathUtilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_namespaces.Utilities
+{
+    internal class MathUtilities
+    {
+        public static void Add(int a,int b)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           var data = new List<List<int>>();
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine($"Enter Marks for Students :");
+                {
+                    var marks = new List<int>();
+                    for (int j = 1; j <= 3; j++)
+                    {
+                        Console.Write($"Enter marks{j}:");
+                        int mark = int.Parse(Console.ReadLine());
+                        marks.Add(mark);
+
+                    }
+                    data.Add(marks);
+                }
+                Console.ReadKey();
+
+
+                }
+
+
+            }
+
+
+
+
+
+
+            }
+    }
+
