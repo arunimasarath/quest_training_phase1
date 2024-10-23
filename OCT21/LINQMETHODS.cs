e1fa173a82b324d080ce075f6e095d1dd114223a
arunimasarath <arunimasarath2002@gmail.com>
2024-10-22 22:18:24 -0700

    Add files via upload

---
+++ OCT21/LINQMETHODS.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = { 1, 2, 3, -1, 2, 3, -9, 4, 5, 6, 7, };
+            var firstItem = data.First();
+            var firstOrDefault = data.FirstOrDefault();
+
+            var lst = data.Last();
+            var lastOrDefault = data.LastOrDefault();
+
+            var SortedAscending = data.OrderBy(x=> x);
+            var SortedDescending = data.OrderByDescending(x=> x);
+
+            var min = data.Min();
+            var max = data.Max();
+            var minGreaterThanFive = data.Min(x => x > 5);
+            var maxGreaterThanFive = data.Max(x => x > 5);
+
+            var Count = data.Count();
+            var countOfNumbersLessThanFive = data.Count(x=> x<5);
+
+            var distinctElements = data.Distinct();
+
+            var firstFive = data.Take(5);
+            var lastFive = data.Take(5);
+
+            var skipFirstFiveAndTakeRemaining = data.Skip(5);
+            var skipLastFive = data.Skip(5);
+
+
+            var takeWhile = data.TakeWhile(x=> x<5);
+            var skipWhile = data.SkipWhile(x=> x<5);
+
+            var where = data.Where(x=> x<5);
+        }
+    }
+}
---
+++ OCT21/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq1
+{
+    internal class Program
+    {
+        class Person
+        {
+            public string Name { get; set; }
+            public string Country { get; set; }
+            public int Age { get; set; }
+            public string Emails { get; set; }
+        };
+        static void Main(string[] args)
+        {
+            var data = new List<Person>()
+            {
+                new Person { Name= "Person1",Country = "US",Age= 20,Emails = },
+                new Person { Name= "Person2",Country = "India",Age= 30},
+                new Person { Name= "Person3",Country = "US",Age= 40}
+
+
+            };
+            var sortedDescending = data.OrderByDescending(x => x.Age);
+            foreach (var item in sortedDescending)
+            {
+                Console.WriteLine(item.Name);
+               
+                Console.WriteLine(item.Age);
+            }
+            
+        }
+    }
+}
