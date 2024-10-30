187fed68292c8093ada0c5d0cbdf938f4cbf16de
arunimasarath <arunimasarath2002@gmail.com>
2024-10-29 21:24:37 -0700

    Add files via upload

---
+++ OCT28/cricket.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPCricketGame
+{
+    enum DeliveryType
+    {
+        NoBall,
+        Runs,
+        Wicket
+    }
+    
+        class Player
+        {
+            public string Name { get; set; }
+            public int Runs { get; set; }
+            public int Wickets { get;  set; }
+
+            public Player(string name)
+            {
+                Name = name;
+                Runs = 0;
+                Wickets = 0;
+            }
+
+            public void ScoreRuns(int runs)
+            {
+                Runs += runs;
+            }
+
+            public void TakeWicket()
+            {
+                Wickets++;
+            }
+
+            public void DisplayScore()
+            {
+                Console.WriteLine($"{Name}'s Score - Runs: {Runs}, Wickets: {Wickets}");
+            }
+        }
+        class Game
+        {
+            private Player player;
+            private Random random;
+
+            public Game(string playerName)
+            {
+                player = new Player(playerName);
+                random = new Random();
+            }
+
+            public void Start()
+            {
+                Console.WriteLine($"Welcome to the Cricket Game, {player.Name}!");
+
+                for (int over = 1; over <= 6; over++) 
+                {
+                    Console.WriteLine($"Over {over}:");
+                    SimulateOver();
+                }
+
+                player.DisplayScore();
+            }
+
+            private void SimulateOver()
+            {
+                for (int ball = 1; ball <= 6; ball++) 
+                {
+                    DeliveryType delivery = (DeliveryType)random.Next(0, 3); 
+
+                    switch (delivery)
+                    {
+                        case DeliveryType.NoBall:
+                            Console.WriteLine("No Ball! Extra run awarded.");
+                            player.ScoreRuns(1); 
+                            break;
+
+                        case DeliveryType.Runs:
+                            int runs = random.Next(0, 7); 
+                            player.ScoreRuns(runs);
+                            Console.WriteLine($"Player scored {runs} runs.");
+                            break;
+
+                        case DeliveryType.Wicket:
+                            Console.WriteLine("Wicket! The player is out.");
+                            player.TakeWicket();
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
---
+++ OCT28/rockpaperscissor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int HumanScore = 0;
+            int ComputerScore = 0;
+            Random random = new Random();
+
+            Console.WriteLine("Welcome to Rock-Paper-Scissors!");
+
+            while (true)
+            {
+                Console.Write("Choose (rock, paper, scissors): ");
+                string HumanChoice = Console.ReadLine().ToLower();
+
+                string[] choices = { "rock", "paper", "scissors" };
+                
+                int choice = random.Next(3);
+                string computerChoice;
+
+               
+                if (choice == 0) computerChoice = "rock";
+                else if (choice == 1) computerChoice = "paper";
+                else computerChoice = "scissors";
+                Console.WriteLine($"Computer chose: {computerChoice}");
+
+                if (HumanChoice != "rock" && HumanChoice != "paper" && HumanChoice != "scissors")
+                {
+                    Console.WriteLine("Invalid choice, try again.");
+                    continue;
+                }
+
+                if (HumanChoice == computerChoice)
+                {
+                    Console.WriteLine("It's a tie!");
+                }
+                else if ((HumanChoice == "rock" && computerChoice == "scissors") ||
+                         (HumanChoice == "scissors" && computerChoice == "paper") ||
+                         (HumanChoice == "paper" && computerChoice == "rock"))
+                {
+                    Console.WriteLine("You win this round!");
+                    HumanScore++;
+                }
+                else
+                {
+                    Console.WriteLine("Computer wins this round!");
+                    ComputerScore++;
+                }
+
+                Console.WriteLine($"Score - You: {HumanScore}, Computer: {ComputerScore}");
+                if (HumanScore == 10)
+                {
+                    Console.WriteLine("Congratulations! You won!");
+                }
+                else
+                {
+                    Console.WriteLine("Computer wins! Try again.");
+                }
+
+            }
+        }
+    }
+}
